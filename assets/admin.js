5246caab387f779aaf81e65437dfb7d42864ccb2
simbe1 <78383727+simbe1@users.noreply.github.com>
2026-08-30 20:48:20 +0200

    v1.1.0: update assets/admin.js

--- assets/admin.js
+++ assets/admin.js
@@ -36,15 +36,42 @@ jQuery(function ($) {
 
     function updateCounter(field) {
         var len = $('#' + field).val().length;
-        $('#' + field.replace('investoryspot_seo_', '') + '-count').text(len);
+        var counter = $('#' + field.replace('investoryspot_seo_', '') + '-count');
+        counter.text(len);
+        counter.parent().removeClass('count-warning count-danger');
+
+        var limit = field.indexOf('description') !== -1 ? 160 : 60;
+        if (len >= limit) {
+            counter.parent().addClass('count-danger');
+        } else if (len >= limit * 0.8) {
+            counter.parent().addClass('count-warning');
+        }
+    }
+
+    function updateSerp() {
+        var fallbackTitle = $('#investoryspot-serp-item').data('fallback-title') || '';
+        var title = $('#investoryspot_seo_title').val() || fallbackTitle;
+        var desc = $('#investoryspot_seo_description').val();
+
+        if (title.length > 60) {
+            title = title.substring(0, 60) + '...';
+        }
+        if (desc.length > 160) {
+            desc = desc.substring(0, 160) + '...';
+        }
+
+        $('#investoryspot-serp-title').text(title);
+        $('#investoryspot-serp-desc').text(desc || 'Your meta description will appear here.');
     }
 
     $('#investoryspot_seo_title').on('input', function () {
         updateCounter('investoryspot_seo_title');
+        updateSerp();
     });
 
     $('#investoryspot_seo_description').on('input', function () {
         updateCounter('investoryspot_seo_description');
+        updateSerp();
     });
 
     $(document).on('click', '.investoryspot-generate-title-btn', function () {
@@ -209,4 +236,6 @@ jQuery(function ($) {
         html += '</div>';
         $('#investoryspot-seo-results').html(html);
     }
+
+    updateSerp();
 });
